510014548ae14eb15e52b2b2e51ffa301da44725
Cobus Carstens <cobus.carstens@gmail.com>
2013-02-22 16:17:38 +0200

    Polishedand refactored DB schema

--- database/sql/create.sql
+++ database/sql/create.sql
@@ -1,73 +1,123 @@
-CREATE DATABASE b3nchm4rk;
-CREATE USER b3nchm4rk;
+--
+-- Run this script as the 'postgres' user to
+-- create the benchmark database and user.
+-- 
 
-GRANT ALL ON DATABASE b3nchm4rk TO b3ncm4rk;
+DROP DATABASE IF EXISTS benchmark;
+DROP USER IF EXISTS benchmark;
+
+CREATE DATABASE benchmark
+       ENCODING 'UTF8'
+       TEMPLATE template0;
+
+CREATE USER benchmark;
+
+GRANT ALL ON DATABASE benchmark TO benchmark;
+
+--
+-- Create template tables using the 'benchmark' user
+--
+
+\connect benchmark;
+SET ROLE benchmark;
 
 
 CREATE TABLE hash_algorithm 
 (
-    id SERIAL PRIMARY KEY,                                -- The hash algorithm ID
-    name VARCHAR NOT NULL UNIQUE                          -- A name describing the hash algorithm
+    id SERIAL PRIMARY KEY,                                
+    name VARCHAR NOT NULL UNIQUE                          
 );
 
-CREATE TABLE user 
+COMMENT ON COLUMN hash_algorithm.id IS 'The hash algorithm ID';
+COMMENT ON COLUMN hash_algorithm.name IS 'A name describing the hash algorithm';
+
+CREATE TABLE member 
 (
-    id BIGSERIAL PRIMARY KEY,                             -- The internal user ID
-    username VARCHAR NOT NULL UNIQUE,                     -- The username used to identify and address the user
-    email_address VARCHAR NOT NULL UNIQUE,                -- The email address used for password resets and other communique
-    salt VARCHAR NOT NULL,                                -- The salt used to hash the password
-    hash_algorithm INTEGER REFERENCES hash_algorithm(id), -- The hash algorithm used to hash the password
-    password_hash VARCHAR NOT NULL,                       -- The result of hash_algorithm(salt+password)
-    external_id VARCHAR NOT NULL UNIQUE                   -- Some character string Used in external links to identify the user
+    id BIGSERIAL PRIMARY KEY,                             
+    username VARCHAR NOT NULL UNIQUE,                     
+    email_address VARCHAR NOT NULL UNIQUE,                
+    salt VARCHAR NOT NULL,                                
+    hash_algorithm INTEGER REFERENCES hash_algorithm(id), 
+    password_hash VARCHAR NOT NULL,                       
+    external_id VARCHAR NOT NULL UNIQUE                   
 );
 
-CREATE TABLE asset_category
+COMMENT ON COLUMN member.id IS 'The internal user ID';
+COMMENT ON COLUMN member.username IS 'The username used to identify and address the member';
+COMMENT ON COLUMN member.email_address IS 'The email address used for password resets and other communique';
+COMMENT ON COLUMN member.salt IS 'The salt used to hash the password';
+COMMENT ON COLUMN member.hash_algorithm IS 'The hash algorithm used to hash the password';
+COMMENT ON COLUMN member.external_id IS 'Some character string used in external links to identify the member';
+
+--
+-- The category table contains the base definitions required by other category tables
+--
+
+CREATE TABLE category
 (
-    id SERIAL PRIMARY KEY,                                -- The asset category ID
-    name VARCHAR NOT NULL UNIQUE,                         -- The name of the asset category
-    description VARCHAR NOT NULL                          -- A description of the asset category
+    id SERIAL PRIMARY KEY,                                
+    name VARCHAR NOT NULL UNIQUE,                         
+    description VARCHAR NOT NULL                          
 );
+
+COMMENT ON COLUMN category.id IS 'The category ID';
+COMMENT ON COLUMN category.name IS 'The name of the category';
+COMMENT ON COLUMN category.description IS 'A description of the category';
+
+CREATE TABLE asset_category 
+(
+    PRIMARY KEY (id),
+    UNIQUE(name)
+)
+INHERITS (category);
+
+COMMENT ON COLUMN asset_category.id IS 'The asset category ID';
+COMMENT ON COLUMN asset_category.name IS 'The name of the asset category';
+COMMENT ON COLUMN asset_category.description IS 'A description of the asset category';
 
 CREATE TABLE liability_category
 (
-    id SERIAL PRIMARY KEY,                                -- The debt category ID
-    name VARCHAR NOT NULL UNIQUE,                         -- The name of the debt category
-    description VARCHAR NOT NULL                          -- A description of the debt category
-);
+    PRIMARY KEY (id),
+    UNIQUE(name)
+)
+INHERITS (category);
 
+COMMENT ON COLUMN liability_category.id IS 'The liability category ID';
+COMMENT ON COLUMN liability_category.name IS 'The name of the liability category';
+COMMENT ON COLUMN liability_category.description IS 'A description of the liability category';
 
-CREATE TABLE user_asset
+CREATE TABLE member_asset
 (
-    user_id BIGINT REFERENCES user(id),
-    asset_category_id REFERENCES asset_category(id),
+    member_id BIGINT REFERENCES member(id),
+    asset_category_id INTEGER REFERENCES asset_category(id),
     value INTEGER NOT NULL DEFAULT 0,
     notes VARCHAR NOT NULL DEFAULT ''
 );
 
-CREATE INDEX user_asset_user_asset_category_index
-ON user_asset(user_id, asset_category_id);
+CREATE INDEX member_asset_member_asset_category_index
+ON member_asset(member_id, asset_category_id);
 
-CREATE INDEX user_asset_user_index
-ON user_asset(user_id);
+CREATE INDEX member_asset_member_index
+ON member_asset(member_id);
 
-CREATE INDEX user_asset_asset_category_index
-ON user_asset(asset_category_id);
+CREATE INDEX member_asset_asset_category_index
+ON member_asset(asset_category_id);
 
-CREATE TABLE user_liability
+CREATE TABLE member_liability
 (
-    user_id BIGINT REFERENCES user(id),
-    liability_category_id REFERENCES liability_category(id),
+    member_id BIGINT REFERENCES member(id),
+    liability_category_id INTEGER REFERENCES liability_category(id),
     value INTEGER NOT NULL DEFAULT 0,
     notes VARCHAR NOT NULL DEFAULT ''
 );
 
-CREATE INDEX user_liability_user_liability_category_index
-ON user_liability(user_id, liability_category_id);
+CREATE INDEX member_liability_member_liability_category_index
+ON member_liability(member_id, liability_category_id);
 
-CREATE INDEX user_liability_user_index
-ON user_liability(user_id);
+CREATE INDEX member_liability_member_index
+ON member_liability(member_id);
 
-CREATE INDEX user_liability_liability_category_index
-ON user_liability(liability_category_id);
+CREATE INDEX member_liability_liability_category_index
+ON member_liability(liability_category_id);
 
 
